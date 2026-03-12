5a0fdabc1b9c2a2b316fdf0514e31cc93aed71e2
Bhavani82510 <bhavanidaram82@gmail.com>
2026-03-12 11:41:51 +0530

    Add files via upload

---
+++ numbersign.js
@@ -0,0 +1,10 @@
+console.log("check the number is positive,negative or zero")
+let n=25
+if(n>0){
+    console.log("number is positive")
+}
+   else if(n<0){ console .log("number is negative")
+}
+else{
+    console.log("zero")
+}
